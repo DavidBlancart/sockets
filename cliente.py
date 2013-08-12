cf6d4394039a16117d2a2a6c2063522fe448eda5
Gustavo Aka Flipjack <gustavoakaflipjack@MacBook-Pro-de-Gustavo.local>
2013-08-12 11:51:29 -0500

    12.08.13

---
+++ cliente.py
@@ -0,0 +1,15 @@
+#encoding:utf-8
+import socket  
+  
+s = socket.socket()   
+s.connect(("localhost", 8000))  
+  
+while True:  
+      mensaje = raw_input("> ")  
+      s.send(mensaje)  
+      if mensaje == "quit":  
+         break  
+  
+print "adios"  
+  
+s.close() 
